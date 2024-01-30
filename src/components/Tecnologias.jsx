eb0860a9ada5c5857f8729c0250d5999f7a147f8
Daniel Posadas Rangel <95835551+DanielPosR@users.noreply.github.com>
2024-01-30 12:50:57 -0600

    agregando imagen de proyecto helm

--- src/components/Tecnologias.jsx
+++ src/components/Tecnologias.jsx
@@ -122,7 +122,6 @@ export default function Tecnologias() {
                                 }}
                             </VisibilitySensor>
 
-
                         </div>
                     )
 
